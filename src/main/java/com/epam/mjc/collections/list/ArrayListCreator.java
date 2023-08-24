9c2aacc82045aec2e0ea595319ab5e781752fc58
Yerke Nurgali <70740293+qelephant@users.noreply.github.com>
2023-08-24 14:52:48 +0600

    Update ArrayListCreator.java

--- src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
@@ -5,5 +5,16 @@ import java.util.List;
 
 public class ArrayListCreator {
     public ArrayList<String> createArrayList(List<String> sourceList) {
+        ArrayList<String> result = new ArrayList<>();
+        
+        for (int i = 0; i < sourceList.size(); i++) {
+            if ((i + 1) % 3 == 0) {
+                String word = sourceList.get(i);
+                result.add(word);
+                result.add(word); // Add the word twice
+            }
+        }
+        
+        return result;
     }
 }
