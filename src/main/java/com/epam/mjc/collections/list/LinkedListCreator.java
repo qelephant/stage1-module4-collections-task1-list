ac916cf5e7f384f5efa99b32c36ad56be0a6c0ad
Yerke Nurgali <70740293+qelephant@users.noreply.github.com>
2023-08-24 14:56:16 +0600

    Update LinkedListCreator.java

--- src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
@@ -5,5 +5,16 @@ import java.util.List;
 
 public class LinkedListCreator {
     public LinkedList<Integer> createLinkedList(List<Integer> sourceList) {
+        LinkedList<Integer> result = new LinkedList<>();
+        
+        for (Integer num : sourceList) {
+            if (num % 2 == 0) {
+                result.addLast(num); // Even number, add to the end
+            } else {
+                result.addFirst(num); // Odd number, add to the beginning
+            }
+        }
+        
+        return result;
     }
 }
